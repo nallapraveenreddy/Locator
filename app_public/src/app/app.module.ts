f100d212fa7ac99d9c4eea9cdce778ef95d50414
nallapraveenreddy <nallapraveenreddy5@gmail.com>
2023-10-02 16:57:27 -0400

    created app_public and installed angular

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -1,47 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { RouterModule }   from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { HomeListComponent } from './home-list/home-list.component';
-import { RatingStarsComponent } from './rating-stars/rating-stars.component';
-import { DistancePipe } from './distance.pipe';
-import { FrameworkComponent } from './framework/framework.component';
-import { AboutComponent } from './about/about.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { PageHeaderComponent } from './page-header/page-header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { HtmlLineBreaksPipe } from './html-line-breaks.pipe';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    HomeListComponent,
-    RatingStarsComponent,
-    DistancePipe,
-    FrameworkComponent,
-    AboutComponent,
-    HomepageComponent,
-    PageHeaderComponent,
-    SidebarComponent,
-    HtmlLineBreaksPipe
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        component: HomepageComponent
-      },
-      {
-        path: 'about',
-        component: AboutComponent
-      }
-    ])
+    BrowserModule
   ],
   providers: [],
-  bootstrap: [FrameworkComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- app_public/src/app/html-line-breaks.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HtmlLineBreaksPipe } from './html-line-breaks.pipe';
-
-describe('HtmlLineBreaksPipe', () => {
-  it('create an instance', () => {
-    const pipe = new HtmlLineBreaksPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
